4b26a0f3f1777445244037494c04180fd638ca67
Tim Jones <tim.jones@forensic-testing.co.uk>
2019-05-29 01:57:12 +0100

    Fixing settings.STATIC_ROOT config

--- goodnewsbot/settings.py
+++ goodnewsbot/settings.py
@@ -180,7 +180,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'goodnewsbot_worker/static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 
 DATE_INPUT_FORMATS = ["%Y-%m-%d", "%d/%m/%Y"]
